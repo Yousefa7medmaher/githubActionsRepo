b5f8a97bc92de6e862450efef349f98c4723b4b1
yousef ahmed <ya1770620@gmail.com>
2026-02-09 07:31:06 +0200

    test changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ function greet(name) {
 }
 
 module.exports = greet 
-if (require.main == module ){ 
-    console.log("World");
-}
+// if (require.main == module ){ 
+//     console.log("World");
+// }
